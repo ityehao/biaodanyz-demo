3aca77a6bf0868c63bae497d649027b0b867f320
ityehao <ityehao@163.com>
2016-10-25 11:33:28 +0800

    first commit

---
+++ 表单验证/07register.php
@@ -0,0 +1,10 @@
+<?php
+        header('Content-Type:text/html;charset=utf-8');
+
+        /*
+              如果是做实际的开发，我这边获取到用户是手机号。
+         把手机号，验证码发送给电信的运营商，让它帮我们把这些消息通过他们的网关发送出去。
+        */
+        echo 'success';
+
+?>
---
+++ 表单验证/registerform.php
@@ -0,0 +1,11 @@
+<?php
+        header('Content-Type:text/html;charset=utf-8');
+        $data=$_GET;
+
+        /*
+           Sleep 休眠
+        */
+        sleep(5);
+        var_dump($data);
+
+?>
